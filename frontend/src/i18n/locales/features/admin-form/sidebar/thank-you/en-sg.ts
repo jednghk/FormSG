5fdbf25fb7cbbe530769d01ec32e5aaca1efb130
Jed Ng <jednghk@gmail.com>
2024-07-11 22:00:32 +0800

    feat(i18n): add admin-form feature directory
    Per discussions, create a nested dir structure for translations
    pertaining to the admin form.
    
    - create admin-form under features
      - nest sidebar and navbar, moving the latter from admin-form-navbar
    - update types and index to reflect changes

---
+++ frontend/src/i18n/locales/features/admin-form/sidebar/thank-you/en-sg.ts
@@ -0,0 +1,12 @@
+import { ThankYou } from '.'
+
+export const enSG: ThankYou = {
+  thankYouPage: {
+    title: 'Customise Thank you page',
+    buttonText: 'Button text',
+    defaultButtonText: 'Submit another response',
+    followUpInstruction: 'Follow-up instruction',
+    redirectLink: 'Button redirect link',
+    defaultRedirectLink: 'Default form link',
+  },
+}
