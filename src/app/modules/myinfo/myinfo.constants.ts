f67732f4609da823ace6dc21751d6c37f6b243c5
Ken Lee Shu Ming <ken@open.gov.sg>
2024-10-28 11:46:19 +0800

    feat(myinfo): update callback referral link (#7560)
    * chore: update myinfo referral link
    
    * feat: add support for backwards compat route

--- src/app/modules/myinfo/myinfo.constants.ts
+++ src/app/modules/myinfo/myinfo.constants.ts
@@ -6,7 +6,9 @@ import { spcpMyInfoConfig } from '../../config/features/spcp-myinfo.config'
  * We make this a constant because it is part of the route
  * registered with MyInfo.
  */
-export const MYINFO_ROUTER_PREFIX = '/myinfo'
+export const MYINFO_ROUTER_PREFIX = '/mi'
+// To support backwards compatibility with the old MyInfo router prefix
+export const LEGACY_MYINFO_ROUTER_PREFIX = '/myinfo'
 
 /**
  * Path to be added after MYINFO_ROUTER_PREFIX for the login
